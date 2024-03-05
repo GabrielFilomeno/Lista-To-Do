da33e9d7edf0816d70eb72bb06cab51301e2c82a
GabrielFilomeno <biel_floripa@hotmail.com>
2024-03-04 22:25:46 -0300

    Update exercicío semana 8

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const ul = document.querySelector("#listaTarefas");
+
+
 function mostrarNumeroTarefas() {
 
     var quantidadeTarefas = document.querySelectorAll(".lista").length;
@@ -7,25 +10,53 @@ function mostrarNumeroTarefas() {
 
 mostrarNumeroTarefas()
 
-function marcarConcluida(checkBox) {
-    var tarefa = checkBox.nextElementSibling;
-    if (checkBox.checked) {
-        tarefa.classList.add('concluida');
-    } else {
-        tarefa.classList.remove('concluida');
-    }
+function marcarConcluida(titulo, idCheckbox, label) {
+    let listaTarefas = lerLista(); 
+
+    listaTarefas.map(tarefa => {
+        if (tarefa.titulo == titulo) {
+            tarefa.concluido = !tarefa.concluido;
+            if (idCheckbox.checked) {
+                label.classList.add('concluida')
+            } else {
+                label.classList.remove('concluida');
+            }
+        };
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(listaTarefas));
+
 }
 
-function removerTarefa(checkboxId) {
+function addClassConcluida(idCheckbox, label){
+listaTarefas = lerLista()
+
+    if (idCheckbox.checked) {
+        label.classList.add('concluida')
+    } else {
+        label.classList.remove('concluida');
+    }
+
+}
+
+function removerTarefa(titulo) {
+
+    let listaTarefas = lerLista();
 
     let confirmar = confirm("Você deseja excluir esta tarefa?");
-
+    
     if (confirmar) {
-        var lista = document.getElementById(checkboxId);
-        var li = lista.parentNode;
-        li.parentNode.removeChild(li);
-    }
+    listaTarefas = listaTarefas.filter(tarefa => {
+        if (tarefa.titulo != titulo) {
+            return true;
+        }
+    })
+}
+
+    localStorage.setItem("tasks", JSON.stringify(listaTarefas));
+
     mostrarNumeroTarefas();
+    renderizarLista();
 }
 
 // Verifica se esta acessando de um celular
@@ -41,7 +72,7 @@ document.body.onresize = function () {
         document.body.style.backgroundPositionX = "0px";
         document.body.style.backgroundPositionY = "0px";
     }
-};  
+};
 
 // Fiz uma função para trocar o tema da lista
 function trocarTema() {
@@ -87,16 +118,34 @@ function trocarTema() {
     };
 };
 
-function adicionarTarefa() {
+function adicionarTarefa(titulo) {
 
-    //Pegando ul do html
-    var ul = document.querySelector("#listaTarefas");
-
-    var addTarefa = document.querySelector("#addTarefa").value;
-    if (addTarefa.trim() === "") {
+    var titulo = document.querySelector("#titulo").value;
+    if (titulo.trim() === "") {
         alert("Digite o nome da tarefa.");
         return;
     } else {
+
+        let tarefa = {
+            titulo: titulo,
+            concluido: false
+        }
+
+        let listaTarefas = lerLista();
+
+        listaTarefas.push(tarefa);
+        localStorage.setItem("tasks", JSON.stringify(listaTarefas));
+
+        document.getElementById("titulo").value = "";
+
+        renderizarLista()
+    }
+}
+function renderizarLista() {
+    var listaTarefas = lerLista();
+    ul.innerHTML = "";
+
+    listaTarefas.map((tarefa, index) => {
 
         //Criando li dentro do ul
 
@@ -109,35 +158,22 @@ function adicionarTarefa() {
         var divLista = document.createElement("div");
         divLista.className = "containerCheckBox";
 
-
-
-
         li.appendChild(divLista);
 
         //Criando input checkbox com label
+        var id = 'tarefa-' + index;
 
         var inputCheckbox = document.createElement("input");
         inputCheckbox.type = "checkbox";
         inputCheckbox.className = "inputCheckBox";
-        var idcheckbox = inputCheckbox.id = Math.random()
+        inputCheckbox.id = id;
         divLista.appendChild(inputCheckbox);
-        divListaId = divLista.id = Math.random()
-
-        console.log(idcheckbox)
 
         var label = document.createElement("label");
-        label.setAttribute('for', idcheckbox);
-        label.textContent = ' ' + addTarefa;
+        label.setAttribute('for', id);
+        label.textContent = ' ' + tarefa.titulo;
 
         divLista.appendChild(label);
-
-
-        //Criando span para texto
-
-
-        // var spanTexto = document.createElement("span");
-        // spanTexto.textContent = addTarefa;
-        // divLista.appendChild(spanTexto);
 
         //Criar botão para remover tarefa
 
@@ -145,25 +181,32 @@ function adicionarTarefa() {
 
         buttonRemover.addEventListener('click', function () {
 
-            divListaId = divLista.id = Math.random(); // alerta 'seuid'
-            console.log(divListaId);
-
-            removerTarefa(divListaId);
+            removerTarefa(tarefa.titulo);
         });
 
         buttonRemover.innerHTML = "<button class='buttonRemover''><i class='fa-solid fa-square-xmark'></i></button>";
         li.appendChild(buttonRemover);
+
+        //Chamando função para marcar novas tarefas como concluidas
+        let idCheckbox = document.getElementById(id)
+        idCheckbox.checked = tarefa.concluido;
+        addClassConcluida(idCheckbox, label);
+        idCheckbox.addEventListener("change", function () {
+
+            marcarConcluida(tarefa.titulo, idCheckbox, label)
+
+        })
+    })
+    mostrarNumeroTarefas();
+}
+function lerLista() {
+    let listaTarefas = localStorage.getItem("tasks");
+
+    if (!!listaTarefas) {
+        return JSON.parse(listaTarefas)
     }
 
-    //Chamando função para marcar novas tarefas como concluidas
-    inputCheckbox.onchange = function () {
-        marcarConcluida(inputCheckbox);
-    };
-
-    //Resetando o valor do addTarefa
-
-    document.getElementById("addTarefa").value = "";
-
-    mostrarNumeroTarefas()
-
+    return [];
 }
+
+renderizarLista();
